06170c4a8b7c0a1d6cb0f9249adf34e79321e394
Adam Beardsley <adam.p.beardsley@gmail.com>
2022-06-04 14:23:47 -0500

    starting an oled test

---
+++ pico_oled/pico_oled.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "../../pico-ssd1306/ssd1306.h"
+#include "../../pico-ssd1306/shapeRenderer/ShapeRenderer.h"
+#include "hardware/i2c.h"
+
+#define LED_PIN 25
+#define I2C_SDA 4
+#define I2C_SCL 5
+
+using namespace pico_ssd1306;
+
+int main() {
+  stdio_init_all();
+  sleep_ms(5000);
+  puts("Hello World1\n");
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  puts("Hello World2\n");
+
+  i2c_init(i2c0, 1000000);
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C_SDA);
+  gpio_pull_up(I2C_SCL);
+
+  puts("Hello World3\n");
+
+  sleep_ms(250);
+  SSD1306 display = SSD1306(i2c0, 0x3D, Size::W128xH32);
+
+  drawLine(&display, 0, 0 ,63, 31);
+  display.sendBuffer();
+  puts("Hello World4\n");
+  while (1) {
+    gpio_put(LED_PIN, 0);
+    sleep_ms(250);
+    gpio_put(LED_PIN, 1);
+    puts("Hello World\n");
+    sleep_ms(1000);
+  }
+}
