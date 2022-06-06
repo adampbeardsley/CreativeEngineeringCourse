7707fcfa3a0c471703febc171d3129b01f9e1337
Adam Beardsley <adam.p.beardsley@gmail.com>
2022-06-06 09:34:58 -0500

    add hooks for rtc and alarm

--- sun_alarm/sun_alarm.cpp
+++ sun_alarm/sun_alarm.cpp
@@ -6,18 +6,21 @@
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/irq.h"
+#include "hardware/rtc.h"
 #include "pico_ws2811.pio.h"
+#include "pico/util/datetime.h"
+
+#define LED_PIN 25
 
 #define NUM_PIXELS 15
-
 #define RESET_TIME_US 400
 #define START_PIN 1
 
 int dma_chan;
-
 uint32_t pixels[NUM_PIXELS*24];
 
-//note  -- this sets the order as RGB. May need to change.
+static volatile bool alarm_fired = false;
+
 void set_pixel_colour(int pixel, uint8_t r, uint8_t g, uint8_t b) {
 
 	uint32_t colour_value = (r << 16 | g << 8 | b);
@@ -37,10 +40,8 @@ int64_t dma_start(alarm_id_t id, void *user_data) {
 	return 0;
 }
 
-//this will be called once a full DMA to the TX Fifo has completed. It just needs to wait and restart
 void dma_handler() {
 	dma_irqn_acknowledge_channel(DMA_IRQ_0,dma_chan);
-
 	//need a pause to re-set the LED strip before we can start again.
 	//can't use sleep in irq handlers as weird things happen
 	add_alarm_in_us(RESET_TIME_US, dma_start, NULL, false);
@@ -53,17 +54,12 @@ void init_ws2811(){
 	uint offset = pio_add_program(pio, &ws2811_program);
 
   ws2811_program_init(pio, sm, offset, START_PIN, 800000);
-
 	dma_chan = dma_claim_unused_channel(true);
-
   dma_channel_config c = dma_channel_get_default_config(dma_chan);
-
 	channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
 	channel_config_set_read_increment(&c, true);
-
 	//See datasheet 2.5.3.1
 	channel_config_set_dreq(&c, DREQ_PIO0_TX0);
-
 	dma_channel_configure(
         dma_chan,
         &c,
@@ -77,20 +73,65 @@ void init_ws2811(){
   dma_channel_set_irq0_enabled(dma_chan, true);
   irq_set_exclusive_handler(DMA_IRQ_0, dma_handler);
   irq_set_enabled(DMA_IRQ_0, true);
-
 	//kick off the initial transfer
 	dma_handler();
 }
 
+static void alarm_isr(void){
+	alarm_fired = true;
+}
+
 int main() {
 
+	stdio_init_all();
 	init_ws2811();
 
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	gpio_put(LED_PIN, 0);
+
+	char datetime_buf[256];
+	char *datetime_str = &datetime_buf[0];
+
+	// Start on Friday 5th of June 2020 15:45:00
+	datetime_t t = {
+					.year  = 2020,
+					.month = 06,
+					.day   = 05,
+					.dotw  = 5, // 0 is Sunday, so 5 is Friday
+					.hour  = 15,
+					.min   = 45,
+					.sec   = 00
+	};
+
+	// Start the RTC
+	rtc_init();
+	rtc_set_datetime(&t);
+
+	datetime_t t0 = {
+					.year  = -1,
+					.month = -1,
+					.day   = -1,
+					.dotw  = -1, // 0 is Sunday, so 5 is Friday
+					.hour  = 15,
+					.min   = 45,
+					.sec   = 10
+	};
+	rtc_set_alarm(&t0, &alarm_isr);
+
+	printf("hello\n");
 	while (true) {
 		for (uint8_t j=0; j<255; j++){
 			for (int i=0; i<NUM_PIXELS;i++) {
 					set_pixel_colour(i,j,0,0);
 				}
+			if (alarm_fired){
+        printf("ALARM!\n");
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        alarm_fired = false;
+      }
 			sleep_ms(100);
 		}
 		for (uint8_t j=255; j>0; j--){
